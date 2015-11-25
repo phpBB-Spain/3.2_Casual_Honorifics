3f5b369240b8444ce119b4b8d69f5450fd9db726
phpBB España <thekuka@gmail.com>
2015-11-25 20:34:29 +0100

    Update mcp.php

--- spanish_casual_honorifics_3.2/language/es-x-tu/mcp.php
+++ spanish_casual_honorifics_3.2/language/es-x-tu/mcp.php
@@ -135,6 +135,7 @@ $lang = array_merge($lang, array(
 	'LOCK_TOPICS_CONFIRM'		=> '¿Estás seguro de querer bloquear los temas seleccionados?',
 	'LOGS_CURRENT_TOPIC'		=> 'Actualmente viendo registros de:',
 	'LOGIN_EXPLAIN_MCP'			=> 'Para moderar este foro debes autenticarte.',
+	'LOGVIEW_VIEWPOST'			=> 'Ver mensaje',
 	'LOGVIEW_VIEWTOPIC'			=> 'Ver tema',
 	'LOGVIEW_VIEWLOGS'			=> 'Ver registro del tema',
 	'LOGVIEW_VIEWFORUM'			=> 'Ver foro',
