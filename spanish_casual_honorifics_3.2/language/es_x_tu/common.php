180b5a45c6d76411bbb01e66d831906ab050ed2d
phpBB España <thekuka@phpbb-es.com>
2017-12-30 12:28:19 +0100

    3.2.2 RC-1 Ready
    3.2.2 RC-1 Ready

--- spanish_casual_honorifics_3.2/language/es_x_tu/common.php
+++ spanish_casual_honorifics_3.2/language/es_x_tu/common.php
@@ -48,7 +48,7 @@ $lang = array_merge($lang, array(
 	'USER_LANG'			=> 'es-x-tu',
 
 	// You can define different rules for the determination of plural forms here.
-	// See http://wiki.phpbb.com/Plural_Rules for more information
+	// See https://area51.phpbb.com/docs/dev/32x/language/plurals.html for more information
 	// or ask the translation manager for help.
 	'PLURAL_RULE'		=> 1,
 
@@ -556,6 +556,7 @@ $lang = array_merge($lang, array(
 	),
 	'OPTIONS'					=> 'Opciones',
 
+	'PAGE_NOT_FOUND'		=> 'La página solicitada no pudo ser encontrada.',
 	'PAGE_OF'				=> 'Página <strong>%1$d</strong> de <strong>%2$d</strong>',
 	'PAGE_TITLE_NUMBER'		=> 'Página %s',
 	'PASSWORD'				=> 'Contraseña',
